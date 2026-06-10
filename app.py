0bb10f7a2bd4675263c33323f4ff0a26819c0d68
Graham King-Abor <grahamkingaborlm10@gmail.com>
2026-06-10 23:38:27 +0100

    Add navigation and fitness pages

--- app.py
+++ app.py
@@ -5,10 +5,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    """
-    Display the home page.
-    """
     return render_template("index.html")
+
+
+@app.route("/workouts")
+def workouts():
+    return render_template("workouts.html")
+
+
+@app.route("/goals")
+def goals():
+    return render_template("goals.html")
+
+
+@app.route("/progress")
+def progress():
+    return render_template("progress.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
